474043348b49e699ef366b7b936bd919c897fc0f
jimduggan <jim.duggan@nuigalway.ie>
2016-06-28 09:19:32 +0100

    Examples for data frames, lists

---
+++ workshop/R code/01 session/04 Lists.R
@@ -0,0 +1,6 @@
+v1<-c(1,2,3)
+v2<-c("One","Two","Three")
+
+l<-list(Numbers=v1,Names=v2)
+
+l1<-list(Extra="Test",l)
---
+++ workshop/R code/01 session/05 Data Frames.R
@@ -0,0 +1,33 @@
+# define three vectors
+time<-seq(1,7) 
+v1<-rep(10,length(time))
+sm<-cumsum(v1)
+
+# group into a data frame
+df<-data.frame(Time=time,Var1=v1,SumVar=sm)
+
+
+# filter by rows
+df[1,]
+df[1:4,]
+df[,1]
+df[,2:3]
+
+# filter using conditional statements
+
+b<-df$SumVar<mean(df$SumVar)
+b
+df[b,]
+
+df[df$SumVar<mean(df$SumVar),]
+
+df$Category<-ifelse(df$SumVar<mean(df$SumVar),"Lower","Higher")
+
+s<-subset(df,df$SumVar>mean(df$SumVar))
+
+# Reading from Excel files
+library(gdata)
+sim <- read.xls("workshop/R code/01 session/SimData.xlsx",
+                stringsAsFactors=FALSE)
+
+
