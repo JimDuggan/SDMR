b5cdaf49865ff21568f6cf74b892d8d3e6c52786
jimduggan <jim.duggan@nuigalway.ie>
2016-07-04 08:41:12 +0100

    Adding sensitivity analysis

---
+++ workshop/models/02 session/03 SIR Sensitivity.R
@@ -0,0 +1,71 @@
+library(deSolve)
+library(ggplot2)
+require(gridExtra)
+library(scales)
+library(FME)
+library(plyr)
+
+START<-0; FINISH<-50; STEP<-0.125; 
+
+simtime <- seq(START, FINISH, by=STEP)
+
+model <- function(time, stocks, auxs){
+  with(as.list(c(stocks, auxs)),{ 
+    aBeta <- aEffective.Contact.Rate / aTotalPopulation
+    aLambda <- aBeta * sInfected
+    
+    fIR <- sSusceptible * aLambda
+    fRR <- sInfected / aDelay
+    
+    dS_dt  <- -fIR
+    dI_dt  <- fIR - fRR
+    dR_dt  <- fRR
+    
+    return (list(c(dS_dt,dI_dt,dR_dt),
+                 IR=fIR, RR=fRR,Beta=aBeta,Lambda=aLambda,DEL=aDelay,
+                 CE=aEffective.Contact.Rate,InitI=initInfected))   
+  })
+}
+
+# setup the parameter data frame
+CE.MIN<-0;          CE.MAX<-7.0
+DEL.MIN<-1.0;       DEL.MAX<-10.0
+INIT.INF.MIN<-1.0;  INIT.INF.MAX<-25.0;
+
+parRange<-data.frame(
+  min=c(CE.MIN, DEL.MIN, INIT.INF.MIN),
+  max=c(CE.MAX, DEL.MAX, INIT.INF.MAX)
+)
+rownames(parRange)<-c("aEffective.Contact.Rate",
+                      "aDelay",
+                      "initInfected")
+
+g.simRuns<-list()
+sensRun<-function(p){
+  
+  g.simRuns<<-list(length=nrow(p))
+
+  for(i in 1:nrow(p)){
+    init<-p[i,"initInfected"]
+
+    a<-c(aTotalPopulation=10000,p[i,1:3])
+    
+    stocks  <- c(sSusceptible=10000-init,sInfected=init,sRecovered=0)
+
+    o<-data.frame(ode(y=stocks, simtime, func = model, 
+                      parms=a, method="euler"))
+    o$run<-i
+    g.simRuns[[i]]<<-o
+  }
+}
+
+NRUNS<-200
+p<-data.frame(Latinhyper(parRange,NRUNS))
+sensRun(p)
+
+df<-rbind.fill(g.simRuns)
+
+p1<-ggplot(df,aes(x=time,y=sInfected,color=run,group=run)) + 
+  geom_line() +
+  ylab("Infected") +
+  xlab("Time (Days)")  + guides(color=FALSE) 
---
+++ workshop/models/02 session/solutions/05 Population Sensitivity.R
@@ -0,0 +1,61 @@
+library(deSolve)
+library(ggplot2)
+library(scales)
+library(FME)
+library(plyr)
+
+START<-2015; FINISH<-2030; STEP<-0.25
+
+simtime <- seq(START, FINISH, by=STEP)
+
+# The model
+model <- function(time, stocks, auxs){
+  with(as.list(c(stocks, auxs)),{ 
+    
+    fPopulationAdded<-sPopulation*aGrowthFraction
+    
+    
+    dP_dt <- fPopulationAdded
+    
+    return (list(c(dP_dt),
+                 Additions=fPopulationAdded,GF=aGrowthFraction))   
+  })
+}
+
+GF.MIN<- -0.07; GF.MAX<-0.07
+
+parRange<-data.frame(
+  min=c(GF.MIN),
+  max=c(GF.MAX)
+)
+rownames(parRange)<-c("aGrowthFraction")
+
+g.simRuns<-list()
+sensRun<-function(p){
+  
+  g.simRuns<<-list(length=nrow(p))
+  
+  for(i in 1:nrow(p)){
+    init<-p[i,"initInfected"]
+    
+    a<-c(aGrowthFraction=p[i,"aGrowthFraction"])
+    
+    stocks  <- c(sPopulation=10000)
+    
+    o<-data.frame(ode(y=stocks, simtime, func = model, 
+                      parms=a, method="euler"))
+    o$run<-i
+    g.simRuns[[i]]<<-o
+  }
+}
+
+NRUNS<-500
+p<-data.frame(Latinhyper(parRange,NRUNS))
+sensRun(p)
+
+df<-rbind.fill(g.simRuns)
+
+p1<-ggplot(df,aes(x=time,y=sPopulation,color=run,group=run)) + 
+  geom_line() +
+  ylab("Population") +
+  xlab("Time (Days)")  + guides(color=FALSE) 
