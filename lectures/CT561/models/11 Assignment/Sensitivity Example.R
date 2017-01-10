cf5224ac1ee518fba2d17585f16d9374bce30a20
jimduggan <jim.duggan@nuigalway.ie>
2017-01-10 15:27:33 +0000

    Update

--- lectures/CT561/models/11 Assignment/Sensitivity Example.R
+++ lectures/CT561/models/11 Assignment/Sensitivity Example.R
@@ -54,20 +54,19 @@ parRange<-data.frame(
 
 rownames(parRange)<-c("aEffective.Contact.Rate","aDelay","initInfected")
 
-NRUNS<-5
+set.seed(1234)
+NRUNS<-250
 p<-data.frame(RunNo=1:NRUNS,Latinhyper(parRange,NRUNS))
 
 out<-apply(p,1,function(x){
-                 #browser()
                  df <- runsim(x)
                  df
                })
 
 df<-rbind.fill(out)
 
-lapply(out,function(x){
+out2<-lapply(out,function(x){
              list(maxI=max(x$sInfected),CE=unique(x$CE))
-  
   })
 
 ggplot(df,aes(x=time,y=sInfected,color=RunNumber)) + 
