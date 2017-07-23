60a540f32cda72cfc5e7b8540284a014eed4fc03
jimduggan <jim.duggan@nuigalway.ie>
2017-07-23 23:02:29 +0100

    Updated files for 3 examples...

--- papers/SD tidyverse/01 Flu Data.R
+++ papers/SD tidyverse/01 Flu Data.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 
 inc <- read_csv("papers/SD tidyverse/data/Incidence.csv")
+coh <- read_csv("papers/SD tidyverse/data/Cohorts.csv")
 
 t_inc <- gather(inc,Cohort,Incidence,Young:Elderly)
 
@@ -9,6 +10,15 @@ ggplot(filter(t_inc,Cohort=="Young"),aes(x=Week,y=Incidence)) + geom_line() + ge
 ggplot(t_inc,aes(x=Week,y=Incidence,color=Cohort)) + geom_line()
 
 ggplot(t_inc,aes(x=Week,y=Incidence,fill=Cohort)) + geom_area()
+
+# Get the total per week and then show the attack rates
+wk_tot <- t_inc %>% group_by(Week) %>%
+             summarise(Incidence=sum(Incidence))
+
+wk_tot <- mutate(wk_tot,AttackRate=100*Incidence/8000)
+
+arrange(wk_tot,desc(Incidence))
+
 
 # get the totals sick by cohort
 t_inc %>% group_by(Cohort) %>% 
